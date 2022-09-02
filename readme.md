da914dde3b6f2a5fff0d252a911e0ba4e16190c4
nbogie <neillbogie@googlemail.com>
2022-09-02 23:35:19 +0100

    settle on one ssl setup for render

--- readme.md
+++ readme.md
@@ -23,3 +23,29 @@ touch index.js
 - add `"start:dev": "ts-node-dev src/index.ts"`
 
 ### create .env file
+
+# DB testing with render
+
+### external address:
+
+x1: ssl: true - YES
+x2: ssl:false - NO: need SSL
+x3: ssl: RU - YES
+
+### internal address:
+
+i1: ssl: true - NO: self signed certificate
+i2: ssl:false - YES
+i3: ssl:RU - YES
+
+### takeaway
+
+So to make a connection from render express to render DB either:
+
+- use external db url and ssl:true
+- use internal db url and ssl: {rejectUnauthorized:false}
+
+# Render typescript build
+
+- build command: `yarn; yarn build`
+- start command: `yarn start`
--- src/index.ts
+++ src/index.ts
@@ -21,113 +21,20 @@ app.get("/", (req, res) => {
   res.send("hello!");
 });
 
+const dbConfig: ClientConfig = {
+  connectionString: getEnvVarOrFail("DATABASE_URL"),
+  ssl: { rejectUnauthorized: false }, //this only for heroku, or render (internal)
+};
+
+const client = new Client(dbConfig);
+client.connect(); //TODO: await this!
+
 // GET /items
-app.get("/items", (req, res) => {
-  res.json([]);
-});
-
-function getEnvVarOrFail(varName: string) {
-  const val = process.env[varName];
-  if (!val) {
-    throw new Error("Missing env var: " + varName);
-  }
-  return val;
-}
-
-const connectionStringInternal = getEnvVarOrFail("DATABASE_URL_INTERNAL");
-const connectionStringExternal = getEnvVarOrFail("DATABASE_URL_EXTERNAL");
-
-app.get("/testI1", async (req, res) => {
-  doAllWithDBConfig(
-    req,
-    res,
-    {
-      connectionString: connectionStringInternal,
-      ssl: true,
-    },
-    "internal, ssl on"
-  );
-});
-
-app.get("/testI2", async (req, res) => {
-  doAllWithDBConfig(
-    req,
-    res,
-    {
-      connectionString: connectionStringInternal,
-      ssl: false,
-    },
-    "internal, ssl false"
-  );
-});
-
-app.get("/testI3", async (req, res) => {
-  doAllWithDBConfig(
-    req,
-    res,
-    {
-      connectionString: connectionStringInternal,
-      ssl: { rejectUnauthorized: false },
-    },
-    "internal, ssl rU"
-  );
-});
-
-app.get("/testX1", async (req, res) => {
-  doAllWithDBConfig(
-    req,
-    res,
-    {
-      connectionString: connectionStringExternal,
-      ssl: true,
-    },
-    "external, ssl true"
-  );
-});
-
-app.get("/testX2", async (req, res) => {
-  doAllWithDBConfig(
-    req,
-    res,
-    {
-      connectionString: connectionStringExternal,
-      ssl: false,
-    },
-    "ext, ssl false"
-  );
-});
-
-app.get("/testX3", async (req, res) => {
-  doAllWithDBConfig(
-    req,
-    res,
-    {
-      connectionString: connectionStringExternal,
-      ssl: { rejectUnauthorized: false },
-    },
-    "ext ssl RU"
-  );
-});
-
-async function doAllWithDBConfig(
-  req: any,
-  res: any,
-  dbConfig: ClientConfig,
-  testLabel: string
-) {
-  //DON'T DO THIS!
-  //We're intentionally testing the db connection INSIDE of the route handler to allow
-  //multiple config tests on the server
-  let client;
-
+app.get("/items", async (req, res) => {
   try {
-    console.log({ dbConfig });
-
-    client = new Client(dbConfig);
-    await client.connect();
     const dbResult = await client.query("select now()");
-    console.log(dbResult.rows);
-    res.send({ msg: testLabel, data: dbResult.rows });
+    console.log("ran query, got", dbResult.rows);
+    res.send({ data: dbResult.rows });
 
     //TODO: this might be a problem if the conn is closed before dbResult.rows is sent to the client.
   } catch (error) {
@@ -138,13 +45,18 @@ async function doAllWithDBConfig(
     } else {
       console.error(message);
     }
-    res.status(500).json({ test: "IN TEST: " + testLabel, error: message });
-  } finally {
-    if (client) {
-      await client.end();
-    }
+    res.status(500).json({ error: message });
   }
+});
+
+function getEnvVarOrFail(varName: string) {
+  const val = process.env[varName];
+  if (!val) {
+    throw new Error("Missing env var: " + varName);
+  }
+  return val;
 }
+
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
 });
