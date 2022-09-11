deac1d1bb969d93026c68cc32de583935c45f030
nbogie <neillbogie@googlemail.com>
2022-09-11 13:29:47 +0100

    add @type and testEnvironment to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   clearMocks: true,
   coverageProvider: "v8",
   preset: "ts-jest",
+  testEnvironment: "node",
 };
